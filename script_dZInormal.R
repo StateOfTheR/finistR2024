140329dd009e9f9a924c3d8d9421d6942436ee2a
Pierre Gloaguen <pierre.gloaguen@univ-ubs.fr>
2024-08-21 10:26:01 +0200

    script fonctionnel

--- script_dZInormal.R
+++ script_dZInormal.R
@@ -1,17 +1,7 @@
-# my_function <- function(x, prob, mu, sigma){
-#   pos_indexes <- which(x != 0)
-#   zero_indexes <- which(x == 0)
-#   n_zeros <- length(x) - length(pos_indexes)
-#   p_term <- sum(log(prob[zero_indexes])) + 
-#     sum(log(1 - prob[pos_indexes]))
-#   mu_term <- 0
-#   if(length(pos_indexes) > 0){
-#     mu_term <- mixtools::logdmvnorm(x[pos_indexes],
-#                                     mu[pos_indexes],
-#                                     sigma[pos_indexes, pos_indexes])
-#   }
-#   p_term + mu_term
-# }
+
+
+library(nimble)
+library(ggmcmc)
 
 dZInormal <- nimbleFunction(
   run = function(x = double(1),
@@ -35,21 +25,14 @@ dZInormal <- nimbleFunction(
     }
     log_output <- p_term + mu_term
     if(log){
-      return(exp(log_output))
+      return(log_output)
     }
     else{
-      return(log_output)
+      return(exp(log_output))
     }
   }
 )
-# 
-# sum(apply(Y, 1, 
-#       function(y) 
-#         dZInormal(y, rep(0.4, 5), mu = 0:4, sigma = diag(1, 5)))) 
-# 
-# sum(apply(Y, 1, 
-#           function(y) 
-#             dZInormal(y, rep(0.4, 5), mu = rep(0, 5), sigma = diag(1, 5)))) 
+
 
 my_code <- nimbleCode({
   for(j in 1:p){
@@ -63,38 +46,47 @@ my_code <- nimbleCode({
 })
 
 registerDistributions(list(
-  dZInormal = list(BUGSdist = "dZInormal(prob, mu, sigma)",
-                   discrete = FALSE, pqAvail = FALSE,
-                   types = c('value = double(1)', 'prob = double(1)', 
-                             'mu = double(1)', 'sigma = double(2)'))
+  dZInormal = list(BUGSdist = "dZInormal(prob, mu, sigma)", # How to call in nimble
+                   discrete = FALSE, # Distribution is not discrete
+                   pqAvail = FALSE, # CDF and quantile function are not available
+                   types = c('value = double(1)', # The random variable is a vector
+                             'prob = double(1)', # a vector
+                             'mu = double(1)', # vector
+                             'sigma = double(2)')) # double(2) is a matrix
 ))
 
-n_obs <- 5000
+
+# Generating data
+set.seed(123)
+n_obs <- 1000
 n_species <- 3
-my_sigma <- diag(1, n_species)
-U <- mixtools::rmvnorm(n = n_obs, mu = 0:(n_species - 1), sigma = my_sigma)
+# Values
+U <- mixtools::rmvnorm(n = n_obs, 
+                       mu = 0:(n_species - 1), 
+                       sigma = diag(1, n_species))
+# Mask (matrix of zeros and ones)
 Z <- rbinom(n = n_obs * n_species, size = 1, prob = .8) %>% 
   matrix(nrow = n_obs)
+# Observations
 Y <- round(U * Z, 9)
 
-my_model <- nimbleModel(code = my_code, 
-                        constants = list(p = n_species, 
-                                         n = n_obs, 
+
+
+my_model <- nimbleModel(code = my_code,
+                        constants = list(p = n_species,
+                                         n = n_obs,
                                          Ip = diag(1, n_species)),
                         data = list(Y = Y),
-                        inits = list(mu = 0:n_species, 
+                        inits = list(mu = rep(0, n_species),
                                      prob = rep(0.5, n_species),
                                      sigma = diag(1, n_species)))
-
 results <- nimbleMCMC(my_model,
-                      nchains = 1, niter = 10000, nburnin = 1000, thin = 10)
-hist(results[,"prob[]"])
+                      samplesAsCodaMCMC = TRUE,
+                      nchains = 2, niter = 10000,
+                      nburnin = 1000, thin = 10)
 
+ggs(results) %>% 
+  ggplot(aes(x = Iteration, y = value, color = factor(Chain))) +
+  facet_wrap(~Parameter, scales = "free") +
+  geom_line()
 
-y <- c(0, 0, 2, 1)
-
-ps <- runif(length(y))
-mus <- rnorm(length(y))
-sigma <- rWishart(n = 1, Sigma = diag(1, length(y)), df = length(y))[,,1]
-my_function(y, ps, mus, sigma)
-dZInormal(y, ps, mus, sigma)
