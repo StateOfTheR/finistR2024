59d4305c71968cacc82ed2d73d3b9282b1d53a33
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2024-08-22 15:40:11 +0200

    Update simulator files.

--- equality_test/eval_functions.R
+++
@@ -1,7 +0,0 @@
-## @knitr metrics
-
-
-pval_loss <- new_metric("p_value", "pval<0.05",
-                       metric = function(model, out) {
-                         return(mean(out$pvalue<0.05))
-                       })
--- equality_test/method_functions.R
+++
@@ -1,6 +0,0 @@
-## @knitr methods
-
-t_test<-new_method("t-test","Mean equality test",
-                   method = function(model,draw){
-                     list(pvalue = t.test(draw[,1], draw[,2],paired=TRUE)$p.value)
-                   })
